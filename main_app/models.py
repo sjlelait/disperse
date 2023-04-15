b67211b8987983220deb81c506216935c24314cb
Sara Le Lait <sjlelait@gmail.com>
2023-04-15 09:53:26 -0600

    wip: adds Spot model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Spot(models.Model):
+    name = models.CharField(max_length=100)
+    zipcode = models.IntegerField(max_length=5)
+    description = models.TextField(max_length=300)
+    latutude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+    def __str__(self):
+        return self.name
