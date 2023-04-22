557a3c2dc7142d8555bf0838dddf4d9830ab4d1b
Sara Le Lait <sjlelait@gmail.com>
2023-04-22 13:59:34 -0600

    fix: heroku access token debugging

--- disperse/settings.py
+++ disperse/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 # mapbox
-MAPBOX_ACCESS_TOKEN = os.getenv('MAPBOX_ACCESS_TOKEN')
+MAPBOX_ACCESS_TOKEN = os.environ['MAPBOX_ACCESS_TOKEN']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
