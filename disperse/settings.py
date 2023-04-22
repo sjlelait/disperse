17056390f6c644a5e34068799373b8f909e4bba1
Sara Le Lait <sjlelait@gmail.com>
2023-04-22 11:18:00 -0600

    wip: start heroku deployment

--- disperse/settings.py
+++ disperse/settings.py
@@ -138,3 +138,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#config django to use heroku's prod. env
+import django_heroku
+django_heroku.settings(locals())
