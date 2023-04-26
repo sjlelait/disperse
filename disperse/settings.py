308f0e95b948576b58e9c222220cf2965a879011
Sara Le Lait <sjlelait@gmail.com>
2023-04-26 15:59:36 -0600

    wip: fixing background image diaplay

--- disperse/settings.py
+++ disperse/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/spots/'
 LOGOUT_REDIRECT_URL = '/'
 
