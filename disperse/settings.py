8aae17b0e814fbd3d0d2e535c46a52c3c2bbb725
Sara Le Lait <sjlelait@gmail.com>
2023-08-23 18:27:04 -0600

    wip: working on background display issues

--- disperse/settings.py
+++ disperse/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_REDIRECT_URL = '/spots/'
 LOGOUT_REDIRECT_URL = '/'
 
