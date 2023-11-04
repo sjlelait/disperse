c264c5d99ed443997ab6b5a577d44d145d7ccce6
Sara Le Lait <sjlelait@gmail.com>
2023-11-04 10:01:44 -0600

    fix: edits login redirect

--- disperse/settings.py
+++ disperse/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-LOGIN_REDIRECT_URL = '/spots/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 #adding for image use
